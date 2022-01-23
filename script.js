52993a03dd53e8244baa45b335b5234bb2691ffe
iliakia <98233436+iliakia@users.noreply.github.com>
2022-01-23 10:45:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-let totalValue = document.querySelector(".p-total-value");
 const button = document.querySelector(".button");
 button.onclick = function(){
-    if(document.querySelector("#price").value>99 && document.querySelector("#price").value<50001 && document.querySelector("#time").value>5 && document.querySelector("#time").value<121){
-        let total = document.querySelector("#price").value*(1+(document.querySelector("#time").value/30));
-        totalValue.innerHTML = total.value;
+    if(document.querySelector("#price").value>99 && document.querySelector("#price").value<50001 && document.querySelector("#time").value>0 && document.querySelector("#bid").value<50 && document.querySelector("#bid").value>0 &&document.querySelector("#time").value<101){ 
+        let total = (document.querySelector("#price").value*1) + (document.querySelector("#price").value*(document.querySelector("#bid").value/100)*document.querySelector("#time").value);
+        document.querySelector("#ptotalvalue").innerHTML = total;
+        console.log(total);
+        
     }
     else{
         alert("Введите корректные данные");
     }
+}
+const clear = document.querySelector(".clear");
+clear.onclick = function(){
+    document.querySelector("#price").value = "";
+    document.querySelector("#time").value = "";
+    document.querySelector("#bid").value = "";
+    total=0;
+    document.querySelector("#ptotalvalue").innerHTML = total;
 }
     
